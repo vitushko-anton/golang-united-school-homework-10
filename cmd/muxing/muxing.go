db78ea22b4fe863f77e9385180acd17a6b1d5eeb
Anton Vitushko <vitushko.anton@gmail.com>
2022-07-09 16:31:33 +0300

    My solutionc homework-10 v0.1

--- cmd/muxing/muxing.go
+++ cmd/muxing/muxing.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -20,6 +21,11 @@ main function reads host/port from env just for an example, flavor it following 
 // Start /** Starts the web server listener on given host and port.
 func Start(host string, port int) {
 	router := mux.NewRouter()
+	router.HandleFunc("/name/{param}", getNameParamHandler).Methods("GET")
+	router.HandleFunc("/bad", getBadHandler).Methods("GET")
+	router.HandleFunc("/data", postDataHandler).Methods("POST")
+	router.HandleFunc("/headers", postHeadersHandler).Methods("POST")
+	router.HandleFunc("/", notDefinedHandler)
 
 	log.Println(fmt.Printf("Starting API server on %s:%d\n", host, port))
 	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), router); err != nil {
@@ -35,4 +41,50 @@ func main() {
 		port = 8081
 	}
 	Start(host, port)
+}
+
+func getNameParamHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	w.WriteHeader(http.StatusOK)
+	response := "Hello, " + vars["param"] + "!"
+	w.Write([]byte(response))
+}
+
+func getBadHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
+}
+
+func postDataHandler(w http.ResponseWriter, r *http.Request) {
+	b, err := io.ReadAll(r.Body)
+	if err == nil {
+		response := "I got message:\n" + string(b)
+		w.Write([]byte(response))
+	}
+}
+
+func postHeadersHandler(w http.ResponseWriter, r *http.Request) {
+	h := r.Header
+
+	if a, ok := h["A"]; ok {
+		if b, ok := h["B"]; ok {
+			ai, err := strconv.Atoi(a[0])
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				w.Write([]byte(err.Error()))
+				return
+			}
+			bi, err := strconv.Atoi(b[0])
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				w.Write([]byte(err.Error()))
+				return
+			}
+			w.Header().Set("a+b", strconv.Itoa(ai+bi))
+			return
+		}
+	}
+	w.WriteHeader(http.StatusOK)
+}
+func notDefinedHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
 }
